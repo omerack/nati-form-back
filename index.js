8a2f9dff6d3d6b26cb5de13110e8d336aca5dea9
omerack <omeracker1@gmail.com>
2023-12-11 16:55:27 +0200

    new changes

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ function sendMail(files) {
 
     const mail_configs = {
       from: "automaticform.gilad@gmail.com",
-      to: "giladac@gmail.com",
+      to: "omeracker1@gmail.com",
       attachments: [
         ...files.map((upload) => ({
           path: upload,
