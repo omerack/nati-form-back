fb7276086ab5d6a4e6346f7aa1c8e9b4833c646b
omerack <omeracker1@gmail.com>
2023-12-12 18:19:15 +0200

    new changes

--- index.js
+++ index.js
@@ -331,10 +331,16 @@ app.get("/preview/:id", async (req, res) => {
 app.get("/bituahLeumi/:id", async (req, res) => {
   const userId = req.params.id;
 
-  fs.readFileSync(`${userId}-bituahLeumi.pdf`);
-  const filePath = path.join(__dirname, `${userId}-bituahLeumi.pdf`);
-  res.set("Content-Type", "application/pdf");
-  res.sendFile(filePath);
+  try {
+    const filePath = path.join(__dirname, `${userId}-bituahLeumi.pdf`);
+    const fileContents = fs.readFileSync(filePath);
+
+    res.set("Content-Type", "application/pdf");
+    res.send(fileContents);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 app.get("/agreement/:id", async (req, res) => {
