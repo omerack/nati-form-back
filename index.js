135e253a4f98139ee7b05a1c8264037bc2dc4395
omerack <omeracker1@gmail.com>
2023-12-17 19:00:08 +0200

    new pdf and changes

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const today = new Date();
 const day = today.getDate();
 const month = today.getMonth();
 const year = today.getFullYear();
-
+const formattedDate = `${day}/${month}/${year}`;
 const findByName = async (dir, name) => {
   const matchedFiles = [];
 
@@ -90,6 +90,7 @@ app.post("/submit", async (req, res) => {
   } else {
     pdfPath = `${directory}/power of attorney - male.pdf`;
   }
+  /* יפוי כוח */
 
   const existingPdf = fs.readFileSync(pdfPath);
   const pdfDoc = await PDFDocument.load(existingPdf);
@@ -101,9 +102,9 @@ app.post("/submit", async (req, res) => {
   const customFont = await pdfDoc.embedFont(fontBytes);
 
   const newPdf = pdfDoc.getPages()[0];
-  newPdf.drawText(name, { x: 460, y: 785, size: 10, font: customFont });
+  newPdf.drawText(name, { x: 420, y: 785, size: 10, font: customFont });
   newPdf.drawText(name, { x: 105, y: 125, size: 12, font: customFont });
-  newPdf.drawText(id, { x: 363, y: 785, size: 11, font: customFont });
+  newPdf.drawText(id, { x: 323, y: 785, size: 11, font: customFont });
   newPdf.drawText(`${day}`, { x: 320, y: 184, size: 11, font: customFont });
   newPdf.drawText(`${year}`, { x: 170, y: 184, size: 11, font: customFont });
   newPdf.drawText(`${monthsNames[month]}`, {
@@ -125,29 +126,60 @@ app.post("/submit", async (req, res) => {
   const modifiedPdf = await pdfDoc.save();
   fs.writeFileSync(`${directory}/${id}-preview.pdf`, modifiedPdf);
 
-  findByName(directory, id).then((files) => {
-    sendMail(files)
-      .then((response) => {
-        console.log(files);
-        files.forEach((file) => {
-          fs.unlink(file, (unlinkErr) => {
-            if (unlinkErr) {
-              console.error(`Error deleting file: ${file}`, unlinkErr);
-            } else {
-              console.log(`Deleted file: ${file}`);
-            }
-          });
-        });
+  /* שכר טרחה */
 
-        res.send({ success: true });
-      })
-      .catch((error) => {
-        console.error(error.message);
-        res
-          .status(500)
-          .send({ success: false, error: "Internal Server Error" });
-      });
+  const feesPdf = fs.readFileSync("./ייפוי כוח.pdf");
+  const feesDoc = await PDFDocument.load(feesPdf);
+
+  feesDoc.registerFontkit(fontkit);
+  const feesFont = await feesDoc.embedFont(fontBytes);
+
+  const feesPageOne = feesDoc.getPages()[0];
+  const feesPageTwo = feesDoc.getPages()[1];
+  feesPageTwo.drawText(name, { x: 400, y: 381, size: 13, font: feesFont });
+  feesPageTwo.drawText(id, { x: 380, y: 347, size: 13, font: feesFont });
+  feesPageOne.drawText(formattedDate, {
+    x: 430,
+    y: 805,
+    size: 15,
+    font: feesFont,
   });
+
+  const pngsignature2 = await feesDoc.embedPng(signature);
+  const pngDims2 = pngsignature.scale(0.2);
+  feesPageTwo.drawImage(pngsignature2, {
+    x: 350,
+    y: 416,
+    width: pngDims2.width,
+    height: pngDims2.height,
+  });
+
+  const feesModified = await feesDoc.save();
+  fs.writeFileSync(`${directory}/${id}-fees.pdf`, feesModified);
+
+    findByName(directory, id).then((files) => {
+      sendMail(files)
+        .then((response) => {
+          console.log(files);
+          files.forEach((file) => {
+            fs.unlink(file, (unlinkErr) => {
+              if (unlinkErr) {
+                console.error(`Error deleting file: ${file}`, unlinkErr);
+              } else {
+                console.log(`Deleted file: ${file}`);
+              }
+            });
+          });
+
+          res.send({ success: true });
+        })
+        .catch((error) => {
+          console.error(error.message);
+          res
+            .status(500)
+            .send({ success: false, error: "Internal Server Error" });
+        });
+    });
 });
 
 app.listen(port, () => {
